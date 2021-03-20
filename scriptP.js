8b5af7ad2851f470818f6e52eead3b898b089c3b
Dhonatan <dhon.joseildo@gmail.com>
2021-03-19 21:52:12 -0300

    update02

---
+++ scriptP.js
@@ -0,0 +1,16 @@
+const Modal = {
+    open(){
+        document
+        .querySelector ('.modal-overlay')
+        .classList
+        .add('active')
+    },
+
+    close(){
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+
+    }
+}
